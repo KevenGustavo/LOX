09bb5751ff7fab1c2e23418e757468d04b1f4e40
arthurswntg2000 <arthur.sampaio@discente.ufma.br>
2025-10-28 23:17:27 -0300

    adicionando Scanner, Token e atualizando o Lox

--- Lox.java
+++ Lox.java
@@ -10,11 +10,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
- static boolean hadError = false;
+  static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); 
+      System.exit(64);
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
@@ -25,15 +26,14 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-
     if (hadError) System.exit(65);
   }
-  
+
   private static void runPrompt() throws IOException {
     InputStreamReader input = new InputStreamReader(System.in);
     BufferedReader reader = new BufferedReader(input);
 
-    for (;;) { 
+    for (;;) {
       System.out.print("> ");
       String line = reader.readLine();
       if (line == null) break;
@@ -50,7 +50,7 @@ public class Lox {
       System.out.println(token);
     }
   }
-  
+
   static void error(int line, String message) {
     report(line, "", message);
   }
---
+++ README.md
@@ -0,0 +1,15 @@
+# Lox — Análise Léxica
+
+
+## Integrantes
+- Arthur Sampaio Pereira
+- Keven Gustavo Dos Santos Gomes
+
+
+## O que está neste repositório
+Arquivos adicionados nesta versão:
+- `Lox.java` — Programa principal
+- `Scanner.java` — Classe que escaneia a entrada e produz tokens.
+- `Token.java` — Representação de tokens.
+- `TokenType.java` — Enum de tipos de token 
+
